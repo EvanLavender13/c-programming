9c2de87ebf43c0d7c87b511ddeac98f9c3c170a5
Evan <evanlavender13@gmail.com>
2021-02-04 21:54:29 -0500

    Doing some pointer stuff

--- src/main.c
+++ src/main.c
@@ -2,6 +2,12 @@
 
 int main(int argc, char **argv) {
     printf("Hello Y'all!\n");
+	
+	int a = 40;
+	int *b = &a;
+	printf("a is %d\n", a);
+	printf("b is %x\n", b);
+	printf("*b is %x\n", *b);
 
     return 0;
 }
