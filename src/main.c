968e9c76094168c45bfeffee4e85789d46f3ad92
Evan <evanlavender13@gmail.com>
2021-02-04 21:57:54 -0500

    Yep

--- src/main.c
+++ src/main.c
@@ -6,8 +6,8 @@ int main(int argc, char **argv) {
 	int a = 40;
 	int *b = &a;
 	printf("a is %d\n", a);
-	printf("b is %x\n", b);
-	printf("*b is %x\n", *b);
+	printf("b is %p\n", b);
+	printf("*b is %d\n", *b);
 
     return 0;
 }
