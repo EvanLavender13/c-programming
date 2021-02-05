b29da3ac2f062061a403e87fa1c132ea35f06127
Evan <evanlavender13@gmail.com>
2021-02-04 22:04:40 -0500

    tabs tabs tabs

--- src/main.c
+++ src/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-	printf("Hello Y'all!\n");
+    printf("Hello Y'all!\n");
 
-	int a = 40;
-	int *b = &a;
-	printf("a is %d\n", a);
-	printf("b is %p\n", b);
-	printf("*b is %d\n", *b);
+    int a = 40;
+    int *b = &a;
+    printf("a is %d\n", a);
+    printf("b is %p\n", b);
+    printf("*b is %d\n", *b);
 
-	return 0;
+    return 0;
 }
